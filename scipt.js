095f84fc511290b46b6035fbe05edf217317fa0b
gpmatos99 <guilhermepmatos@hotmail.com>
2025-07-01 20:17:34 +0100

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -3,110 +3,90 @@ const agrupamentos = {
     nome: "Nova Oeiras",
     emocao: "Tristeza",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_tristeza.png?v=1749844340108",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/piper_tristeza_tlm.png?v=1749913825047",
+      desktop: "Piper_tristeza.png",
+      mobile: "piper_tristeza_tlm.png",
     },
   },
   413: {
     nome: "Ferragudo",
     emocao: "Medo",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_medo.png?v=1749844336784",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_medo_tlm.png?v=1749913823479",
+      desktop: "Piper_medo.png",
+      mobile: "Piper_medo_tlm.png",
     },
   },
   692: {
     nome: "São Jacinto",
     emocao: "Medo",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_medo.png?v=1749844336784",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_medo_tlm.png?v=1749913823479",
+      desktop: "Piper_medo.png",
+      mobile: "Piper_medo_tlm.png",
     },
   },
   235: {
     nome: "Figueira da Foz",
     emocao: "Alegria",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_felicidade.png?v=1749844336091",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_felicidade_tlm.png?v=1749913823872",
+      desktop: "Piper_felicidade.png",
+      mobile: "Piper_felicidade_tlm.png",
     },
   },
   216: {
     nome: "São Martinho",
     emocao: "Alegria",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_felicidade.png?v=1749844336091",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_felicidade_tlm.png?v=1749913823872",
+      desktop: "Piper_felicidade.png",
+      mobile: "Piper_felicidade_tlm.png",
     },
   },
   1100: {
     nome: "Parque das Nações",
     emocao: "Raiva",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_raiva.png?v=1749844339412",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/piper_raiva_tlm.png?v=1749913871073",
+      desktop: "Piper_raiva.png",
+      mobile: "piper_raiva_tlm.png",
     },
   },
   377: {
     nome: "Leça da Palmeira",
     emocao: "Raiva",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_raiva.png?v=1749844339412",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/piper_raiva_tlm.png?v=1749913871073",
+      desktop: "Piper_raiva.png",
+      mobile: "piper_raiva_tlm.png",
     },
   },
   1117: {
     nome: "Setúbal",
     emocao: "Repulsa",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_nojo.png?v=1749844338087",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/piper_nojo_tlm.png?v=1749913082718",
+      desktop: "Piper_nojo.png",
+      mobile: "piper_nojo_tlm.png",
     },
   },
   253: {
     nome: "Seixal",
     emocao: "Vergonha",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_vergonha.png?v=1749844341283",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_vergonha_tlm.png?v=1749913825519",
+      desktop: "Piper_vergonha.png",
+      mobile: "Piper_vergonha_tlm.png",
     },
   },
   1180: {
     nome: "Santa Cruz - Barreiro",
     emocao: "Vergonha",
     img: {
-      desktop:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_vergonha.png?v=1749844341283",
-      mobile:
-        "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/Piper_vergonha_tlm.png?v=1749913825519",
+      desktop: "Piper_vergonha.png",
+      mobile: "Piper_vergonha_tlm.png",
     },
   },
 };
 
+
 const fundoCentroComando = {
-  desktop:
-    "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/allheadquarters.png?v=1749835414750",
-  mobile:
-    "https://cdn.glitch.global/14c4bb69-fbdb-42a4-90b7-7ea8da63c47d/8ad6270f-638f-4b00-80e3-7864ccd02411.png?v=1749918623863",
+  desktop: "allheadquarters.png",
+  mobile: "8ad6270f-638f-4b00-80e3-7864ccd02411.png",
 };
+
 
 function mostrarChapeu() {
   document.getElementById("inicio").style.display = "none";
